9ff0706a64d0360a6c2fe09bfcc1a63a0983beea
midas1214 <hsiung.midas@gmail.com>
2022-12-14 00:59:41 +0800

    far camera added

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -8,25 +8,38 @@ using TMPro;
 public class CameraZoom : MonoBehaviour , IPointerClickHandler
 
 {
-    public Camera Cam;
+    public Camera mainCam;
+    public Camera farCam;
+    public GameObject cam1;
+    public GameObject cam2;
+
+    public Canvas cameraCanvas;
+
     public float zoomSpeed = 2;
     bool zoom = false;
     public TextMeshProUGUI text;
     private void Start()
     {
-        Cam = Camera.main;
+        mainCam = Camera.main;
+        cameraCanvas = gameObject.GetComponentInParent<Canvas>();
     }
     public void OnPointerClick(PointerEventData eventData)
     {
         if (zoom == false)
         {
-            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize,3,zoomSpeed);
+            mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize,3,zoomSpeed);
+            cam2.SetActive(true);
+            cam1.SetActive(false);
             text.text = "地圖放大";
+            cameraCanvas.worldCamera = farCam;
         }
         else
         {
-            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 1.6f, zoomSpeed);
+            mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, 1.6f, zoomSpeed);
+            cam2.SetActive(false);
+            cam1.SetActive(true);
             text.text = "地圖縮小";
+            cameraCanvas.worldCamera = mainCam;
         }
         zoom = !zoom;
     }
